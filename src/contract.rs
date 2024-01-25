bd67722dd195854681d498b7e96efd0891c0766b
Timur Sefergil <sefergil29@gmail.com>
2024-01-25 16:56:55 +0300

    Added states

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,11 @@
-use crate::msg::ValueResp;
-
 pub mod query {
     use crate::msg::ValueResp;
+    use crate::state::COUNTER;
+    use cosmwasm_std::{Deps, StdResult};
 
-    pub fn value() -> ValueResp {
-        ValueResp { value: 0 }
+    pub fn value(deps: Deps) -> StdResult<ValueResp> {
+        let value = COUNTER.load(deps.storage)?;
+        Ok(ValueResp { value })
     }
 
     pub fn incremented(val: u64) -> ValueResp {
